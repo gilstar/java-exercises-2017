fc1495a24125dfb27aa219549a2771305426c798
yoav lo <yoavlongman@gmail.com>
2017-08-10 15:38:10 +0300

    s

--- src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
+++ src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
@@ -10,6 +10,7 @@ public class CollectionsPlayground {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("Start list collection");
         ArrayList<String> stringList = new ArrayList<String>();
         stringList.add("hello");
